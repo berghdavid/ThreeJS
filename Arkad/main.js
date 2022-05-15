a68000c57e0a5c9deb40268fc5195f3c140a31a1
David Bergh <berghdavid@hotmail.com>
2022-05-15 16:19:18 +0200

    Refactor

--- Arkad/main.js
+++ Arkad/main.js
@@ -11,17 +11,11 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 });
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 2, -30)
+controls.target.set(0, 6, -30)
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-
-// Donut
-const geometry = new THREE.TorusGeometry(10, 1, 16, 100);
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347 });
-const torus = new THREE.Mesh(geometry, material);
-//scene.add(torus);
 
 // Light sources
 const pointLight = new THREE.PointLight(0xffffff);
@@ -36,13 +30,15 @@ scene.add(gridHelper);
 
 
 // Background
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('assets/images/space.jpg');
 scene.background = spaceTexture;
 
 // Arkad board
 const plaque = new THREE.Mesh(
   new THREE.PlaneGeometry(32, 32),
   new THREE.MeshStandardMaterial({
+    transparent: true,
+    opacity: 0.8,
     color: 0xFFFFFF
   })
 );
@@ -51,12 +47,12 @@ scene.add(plaque)
 
 // Arkad logo
 loader.load(
-  'arkad.gltf',
+  'assets/models/arkad.gltf',
   function (gltf) {
     gltf.scene.scale.set(32, 32, 32)
     gltf.scene.rotateX(Math.PI / 2)
     gltf.scene.position.x = -11.5
-    gltf.scene.position.y = 24
+    gltf.scene.position.y = 23
     gltf.scene.position.z = -30
 
     // Change material from darker to lighter
@@ -75,50 +71,26 @@ loader.load(
   }
 )
 
-// Image of me
-const meTexture = new THREE.TextureLoader().load('PG.jpg');
-const me = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: meTexture }));
-//scene.add(me);
-
-// Moon
-const moonTexture = new THREE.TextureLoader().load('Cube.jpg');
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
+// Earth
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(40, 32, 32),
   new THREE.MeshStandardMaterial({
-    map: moonTexture,
+    map: new THREE.TextureLoader().load('assets/images/earth.jpg')
   })
 );
-moon.position.x = -10;
-moon.position.z = 15;
-scene.add(moon);
-
-// House
-const materialTransparent =  new THREE.MeshBasicMaterial({
-  transparent: true,
-  opacity: 0,
-  wireframe: true,
-  side: THREE.DoubleSide
-});
-const boxMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-var materials = [ materialTransparent, boxMaterial, boxMaterial, boxMaterial, boxMaterial, boxMaterial ]
-const geometry2 = new THREE.BoxGeometry(20, 20, 20);
-const cube = new THREE.Mesh(geometry2, materials);
-cube.position.x = 20;
-scene.add(cube);
-
+earth.position.set(0, 13, -80)
+scene.add(earth);
 
 // Camera scrolling
+const flyZ = 32;
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
 
-  me.rotation.y += 0.01;
-  me.rotation.z += 0.01;
+  camera.position.set(0, 1, 5 - t * 0.02)
 
-  camera.position.set(0, 0.4, 5 - t * 0.01)
-
-  if(camera.position.z > 24) {
-    let diff = camera.position.z - 24
-    let modifier = diff*0.01 + Math.min(diff, 10)*diff*0.1;
+  if(camera.position.z > flyZ) {
+    let diff = camera.position.z - flyZ
+    let modifier = diff*0.08 + Math.min(diff, 8)*diff*0.08;
     camera.position.x -= modifier;
     camera.position.y += modifier;
   }
@@ -130,12 +102,9 @@ moveCamera();
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.001;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  earth.rotation.y -= 0.0014;
 
   controls.update();
-
   renderer.render(scene, camera);
 }
 
